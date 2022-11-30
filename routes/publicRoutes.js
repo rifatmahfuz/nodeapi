6591881416c6a3b6206f500adde6e08d33834bfa
Rifat Al Mahfuz <mahfuz@sharetrip.net>
2022-11-30 15:08:22 +0600

    added authorization

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const authorize = require("../middlewares/authorize");
+
 const con = require("../controllers/coreController");
 const user = require("../controllers/userController");
 
 router.post("/register", con.register);
 router.post("/login", con.login);
-router.get("/users/:id", user.getOneUser);
-router.delete("/users/:id", user.deleteOneUser);
-router.put("/users/:id", user.updateOneUser);
+router.get("/users/:id", authorize, user.getOneUser);
+router.delete("/users/:id", authorize, user.deleteOneUser);
+router.put("/users/:id", authorize, user.updateOneUser);
 
 module.exports = router;
